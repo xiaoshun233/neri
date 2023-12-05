4e2db059586240baefeae7bf38c401a23550b8e4
nerisama <228745244@qq.com>
2023-12-05 13:00:46 +0800

    1.03

--- php/interface/login.php
+++ php/interface/login.php
@@ -1,16 +1,29 @@
 <?php
-    if (isset($_POST['data'])) {
-        include '../class/User.class.php';
+try{
+    $result =  ["result"=>"","status"=>false,"msg"=>""];
+    if (!isset($_POST['data'])) {
+        throw new Exception('No get data in this login post request');
+    }
+    include '../class/User.class.php';
         $data = json_decode($_POST['data']); 
         $user = new User();
         $user ->set('username',$data->username);
         $user ->set('password',$data->password);
         $data = null;
-        $result = $user ->login();
-        $user = null;
-        echo json_encode($result);
-    }
-    else{
-        die('No get data in this login post request');
-    }
+        if($result['result'] = $user ->login()){
+            $result['result'] = getToken('usertoken');
+        }
+        else{
+            throw new Exception('登录失败');
+        }
+        $result['status'] = true;
+        $result['msg'] = '登陆成功';
+}
+catch(Exception $e){
+    $result['msg'] = $e -> getMessage();
+}
+finally{
+    echo json_encode($result);
+}       
+
 ?>
